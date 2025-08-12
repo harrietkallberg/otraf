0fae5b9910bcfe046a991b4bcea8e171b6e17d67
harrietkallberg <120216648+harrietkallberg@users.noreply.github.com>
2025-08-12 22:01:31 +0200

    Update supabaseClient.ts

--- transit-webapp/frontend/src/supabaseClient.ts
+++ transit-webapp/frontend/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uaqfokbeqneynggspqwk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhcWZva2JlcW5leW5nZ3NwcXdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NTQwMjIsImV4cCI6MjA2OTAzMDAyMn0.m04qZFLCqgHZGCpFjWVGrX6mxskdlsARsul7MStgV_8'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
